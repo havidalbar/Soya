8fdda05db109772391b85dd449d55acf78ebc310
Alfa <alfafadila27@gmail.com>
2019-09-30 19:33:55 +0700

    Tambah activity dan layout splash

--- app/src/main/java/com/example/soya/MainActivity.kt
+++ app/src/main/java/com/example/soya/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.soya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent;
+import android.os.Handler;
+import android.R.id.home
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val waktuloading = 4000
+
+    //4000=4 detik
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed(Runnable {
+            //setelah loading maka akan langsung berpindah ke home activity
+            val home = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(home)
+            finish()
+        }, waktuloading.toLong())
     }
 }

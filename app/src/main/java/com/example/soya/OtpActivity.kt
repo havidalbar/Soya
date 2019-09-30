c9e19cc171f778ea0fdfc9095ad81db406e8ad6b
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2019-09-30 23:29:41 +0700

    Layout otw

--- app/src/main/java/com/example/soya/OtpActivity.kt
+++ app/src/main/java/com/example/soya/OtpActivity.kt
@@ -3,10 +3,10 @@ package com.example.soya
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OtpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_otp)
     }
 }

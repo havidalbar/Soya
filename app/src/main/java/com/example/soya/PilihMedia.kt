93c7aa8fe01bcdf48d32ff5605af5f3f9321c17d
Alfa <alfafadila27@gmail.com>
2019-10-01 16:18:34 +0700

    pilih media

---
+++ app/src/main/java/com/example/soya/PilihMedia.kt
@@ -0,0 +1,12 @@
+package com.example.soya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PilihMedia : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilih_media)
+    }
+}

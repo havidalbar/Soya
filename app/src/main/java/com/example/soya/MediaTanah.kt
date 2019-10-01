33699de90632f56b0a1c1716be8f38b4ac570b18
Alfa <alfafadila27@gmail.com>
2019-10-02 01:03:11 +0700

    media tanah

---
+++ app/src/main/java/com/example/soya/MediaTanah.kt
@@ -0,0 +1,12 @@
+package com.example.soya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MediaTanah : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_tanah)
+    }
+}

98d3e70343d81a80fc591fc81e6ecc0d5fa40801
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2019-10-03 04:33:41 +0700

    Recyclerview pilih jenis tanah

---
+++ app/src/main/java/com/example/soya/Activities/PilihJenisTanahActivity.kt
@@ -0,0 +1,35 @@
+package com.example.soya.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.soya.Adapters.ListPilihJenisTanahAdapter
+import com.example.soya.R
+import kotlinx.android.synthetic.main.activity_pilih_jenis_tanah.*
+
+data class PilihJenisTanah(
+    val namaJenisTanah: String,
+    val gambarJenisTanah: String
+)
+
+class PilihJenisTanahActivity : AppCompatActivity() {
+
+    val pilihJenisTanah: ArrayList<PilihJenisTanah> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilih_jenis_tanah)
+
+        addRekomendasiIkans()
+
+        rv_jenis_tanah_list.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ListPilihJenisTanahAdapter(pilihJenisTanah)
+        }
+    }
+
+    fun addRekomendasiIkans() {
+        pilihJenisTanah.add(PilihJenisTanah("Tanah Liat","tanahliat"))
+        pilihJenisTanah.add(PilihJenisTanah("Tanah Berpasir","tanahberpasir"))
+    }
+}
